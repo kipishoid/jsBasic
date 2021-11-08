747bfdc5443eea70277561875241acc23ae24208
[sivolapov_max] <[kipish.max@mail.ru]>
2021-11-08 23:25:04 +0300

    task6

---
+++ level2/task6/js/filter.js
@@ -0,0 +1,13 @@
+Vue.component('my-filter', {
+  data() {
+    return {
+      userSearch: ''
+    }
+  },
+  template: `
+        <form action="#" class="cart__form_search" @submit.prevent="$parent.filter(userSearch)">
+          <input type="text" class="cart__form_search_input" v-model="userSearch">
+          <button class="cart__btn-searth" type="button">Поиск</button>
+        </form>
+  `
+});
